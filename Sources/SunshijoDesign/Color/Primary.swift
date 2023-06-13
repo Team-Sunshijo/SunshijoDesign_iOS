a73eecb349290a27d43c04967580428823671b9a
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-06-13 17:04:06 +0900

    💄 :: main4 Add

--- Sources/SunshijoDesign/Color/Primary.swift
+++ Sources/SunshijoDesign/Color/Primary.swift
@@ -8,4 +8,5 @@ public extension Color.Primary {
     static let main:  Color = #colorLiteral(red: 1, green: 0.7383009791, blue: 0.1349614859, alpha: 1).color
     static let main2: Color = #colorLiteral(red: 0.996150434, green: 0.8249177933, blue: 0.4048083425, alpha: 1).color
     static let main3: Color = #colorLiteral(red: 1, green: 0.7009802461, blue: 0.0008154966054, alpha: 1).color
+    static let main4: Color = #colorLiteral(red: 0.999238193, green: 0.9350250363, blue: 0.7746088505, alpha: 1).color
 }
