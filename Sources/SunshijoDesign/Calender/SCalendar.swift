4411a4de2bca7ec233c09b2fc831de367e492cf2
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-06-22 22:49:59 +0900

    ✨ ::  주말 색깔설정

--- Sources/SunshijoDesign/Calender/SCalendar.swift
+++ Sources/SunshijoDesign/Calender/SCalendar.swift
@@ -18,8 +18,14 @@ struct SCalendar: View {
         let daysInPrevMonth = prevMonth.getWeeksCountOfMonth()
 
         VStack{
+            HStack(spacing: 35) {
+                ForEach(weeks, id: \.self) { index in
+                    Text(index)
+                        .foregroundColor(index == "일" ? .red : (index == "토" ? .blue : .GrayScale.gray900))
+                }
+            }
             ForEach(0..<weeksCountInMonth, id: \.self) { row in
-                HStack(spacing: 28) {
+                HStack(spacing: 30) {
                     ForEach(1..<8, id:\.self) { day in
                         let count = (day + (row * 7))
                         CalendarCell(
