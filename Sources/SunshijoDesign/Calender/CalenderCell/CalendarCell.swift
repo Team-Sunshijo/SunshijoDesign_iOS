f8ef30d2a108ff65dacb2377e1c1a80d861ec9ec
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-06-29 12:14:30 +0900

    일단 커밋

--- Sources/SunshijoDesign/Calender/CalenderCell/CalendarCell.swift
+++ Sources/SunshijoDesign/Calender/CalenderCell/CalendarCell.swift
@@ -10,54 +10,82 @@ struct CalendarCell: View {
     
     
     var body: some View {
-        Text(checkMonthType().dayToString())
-            .sdText(
-                type: .regular16,
-                textColor: textColor(type: checkMonthType().monthType)
-            )
-            .frame(width: checkSize(), height: checkSize())
-    }
-    
-    func textColor(type: MonthType) -> Color {
-        if Int(date.toString("d")) == count - startingSpaces && Date().toString("yyyy MM") == date.toString("yyyy MM") {
-            return .black
-        } else if type == .current {
-            return .black
-        } else {
-            return .GrayScale.gray500
+        if #available(iOS 16.0, *) {
+            return Text(checkMonthType().dayToString())
+                .sdText(
+                    type: .regular16,
+                    textColor: textColor(type: checkMonthType().monthType)
+                )
+                .frame(width: checkSize(), height: checkSize())
+                .background(backgroundColor())
+                .cornerRadius(20)
+                .underline(true, pattern: .solid, color: borderColor())
+                
+        }else {
+            return Text(checkMonthType().dayToString())
+                .sdText(
+                    type: .regular16,
+                    textColor: textColor(type: checkMonthType().monthType)
+                )
+                .frame(width: checkSize(), height: checkSize())
+                .background(backgroundColor())
         }
-    }
-    func checkMonthType() -> Month {
-        if count <= startingSpaces {
-            let day = daysInPrevMonth + count - startingSpaces
-            return Month(monthType: .previous, day: day)
-        } else if count - startingSpaces > daysInMonth {
-            let day = count - startingSpaces - daysInMonth
-            return Month(monthType: .next, day: day)
-        } else {
-            let day = count - startingSpaces
-            return Month(monthType: .current, day: day)
-        }
-    }
-    func borderColor() -> Color {
-        return specialDate.filter {
-            $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("yyyy MM dd") != Date().toString("yyyy MM dd")
-        }
-        .map { Int($0.toString("d")) }
-        .contains { $0 == count - startingSpaces } ? .Primary.main : .white
-    }
-    func checkSize() -> CGFloat {
-        specialDate.filter {
-            $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("dd") != date.toString("dd")
-        }
-        .map { Int($0.toString("d")) }
-        .contains { $0 == count - startingSpaces } ? 20 : 20
-    }
-    
-}
 
-struct CalendarCell_Previews: PreviewProvider {
-    static var previews: some View {
-        CalendarCell(date: Date(), specialDate: [], count: 1, startingSpaces: 1, daysInMonth: 1, daysInPrevMonth: 1)
+        func textColor(type: MonthType) -> Color {
+            if Int(date.toString("d")) == count - startingSpaces && Date().toString("yyyy MM") == date.toString("yyyy MM") {
+                return .black
+            } else if type == .current {
+                return .black
+            } else {
+                return .GrayScale.gray500
+            }
+        }
+
+        func checkMonthType() -> Month {
+            if count <= startingSpaces {
+                let day = daysInPrevMonth + count - startingSpaces
+                return Month(monthType: .previous, day: day)
+            } else if count - startingSpaces > daysInMonth {
+                let day = count - startingSpaces - daysInMonth
+                return Month(monthType: .next, day: day)
+            } else {
+                let day = count - startingSpaces
+                return Month(monthType: .current, day: day)
+            }
+        }
+
+        func borderColor() -> Color {
+            return specialDate.filter {
+                $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("yyyy MM dd") != Date().toString("yyyy MM dd")
+            }
+            .map { Int($0.toString("d")) }
+            .contains { $0 == count - startingSpaces } ? .Primary.main : .white
+        }
+
+        func currentTextColor() -> Color {
+            return specialDate.filter {
+                $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("yyyy MM dd") != Date().toString("yyyy MM dd")
+            }
+            .map { Int($0.toString("d")) }
+            .contains { $0 == count - startingSpaces } ? .Primary.main3 : .black
+        }
+
+        func backgroundColor() -> Color {
+            return Int(date.toString("d")) == count - startingSpaces && Date().toString("yyyy MM") == date.toString("yyyy MM") ? .Primary.main4 : .white
+        }
+
+        func checkSize() -> CGFloat {
+            specialDate.filter {
+                $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("dd") != date.toString("dd")
+            }
+            .map { Int($0.toString("d")) }
+            .contains { $0 == count - startingSpaces } ? 30 : 30
+        }
+    }
+
+    struct CalendarCell_Previews: PreviewProvider {
+        static var previews: some View {
+            CalendarCell(date: Date(), specialDate: [], count: 1, startingSpaces: 1, daysInMonth: 1, daysInPrevMonth: 1)
+        }
     }
 }
